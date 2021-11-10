47eaa39eef7cce835dc802a2ef821cdada84b088
Maharavan <90346511+Maharavan@users.noreply.github.com>
2021-11-10 20:06:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package collections.shopping;
+
+public class Main {
+    public static void main (String [] args) {
+System.out.println("Enter user id");
+int a[]={0,1,2,3,4};
+int a1=in.nextInt();
+System.out.println("Password");
+int p=in.nextInt();
+if(a[a1]==p)
+        new UI();
+else
+System.out.print("Wrong pass");
+        
+    }
+}
